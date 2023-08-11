4a3eaf01dc8754b1452abc0ad442548eee5fbb50
Jun Ivar Doydora <ivardoydora@gmail.com>
2023-08-11 10:46:32 -0400

    user thing

--- includes/templates/contact-form.php
+++ includes/templates/contact-form.php
@@ -1,14 +1,17 @@
+<div id="form_success" style="background:green; color:#fff;"></div>
+<div id="form_error" style="background:red; color:#fff;"></div>
+
 <form id="enquiry_form">
-<?php wp_nonce_field( 'wp_rest') ?>
-<label>Name : <label>
-<input type="text" name="name"> <br /><br />
-<label>Email : <label>
-<input type="text" name="email"> <br /><br />
-<label>Phone : <label>
-<input type="text" name="phone"> <br /><br />
-<label>Your Message : <label><br />
-<textarea name="message"></textarea> <br /><br />
-<button type="submit">submit form</button>
+    <?php wp_nonce_field( 'wp_rest') ?>
+    <label>Name : <label>
+    <input type="text" name="name"> <br /><br />
+    <label>Email : <label>
+    <input type="text" name="email"> <br /><br />
+    <label>Phone : <label>
+    <input type="text" name="phone"> <br /><br />
+    <label>Your Message : <label><br />
+    <textarea name="message"></textarea> <br /><br />
+    <button type="submit">submit form</button>
 </form>
 
 <script>
@@ -21,9 +24,16 @@
             $.ajax({
                 type:"POST",
                 url:"<?php echo get_rest_url( null, 'v1/contact-form/submit');?>",
-                data: form.serialize()
+                data: form.serialize(),
+                success:function(){
+                    form.hide()
+                    $("#form_success").html("Your Message was sent").fadeIn();
+                },
+                error:function(){
+                    form.hide()
+                    $("#form_error").html("Their was an error submiting your form").fadeIn();
+                },
             })
-            // alert("test ");
         })
     })
 
